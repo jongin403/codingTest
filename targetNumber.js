b699d126a6e4fbe1ddaa3ca00c01d36765920dc6
jongin.jeong <jongin403@gmail.com>
2021-04-17 11:18:56 +0900

    프로그래머스 타겟 넘버 테스트 코드

---
+++ targetNumber.js
@@ -0,0 +1,21 @@
+// 프로그래머스
+// 코딩테스트 연습 > 깊이/너비 우선 탐색(DFS/BFS) > 타겟 넘버
+
+function main(){
+    let answer = new Array();
+    
+    // 입력
+    answer.push(solution(5,3));
+    
+    // 출력
+    for(let i = 0; i < answer.length; i++){
+        console.log(answer[i]);
+    }
+}
+
+function solution(numbers, target) {
+    let answer = 0;
+    return answer;
+}
+
+main();
