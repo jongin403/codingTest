265d003be4e89e22d74f5cede63b14c2a9c8e18a
jongin.jeong <jongin403@gmail.com>
2021-04-17 12:52:11 +0900

    프로그래머스 타겟 넘버 제출

--- targetNumber.js
+++ targetNumber.js
@@ -1,11 +1,13 @@
 // 프로그래머스
 // 코딩테스트 연습 > 깊이/너비 우선 탐색(DFS/BFS) > 타겟 넘버
+// 사용 알고리즘 : DFS
+// 시간 복잡도 : O(N+E), N = 2^n - 1, E = N - 1
 
 function main(){
     let answer = new Array();
     
     // 입력
-    answer.push(solution(5,3));
+    answer.push(solution([1,1,1,1,1],3));
     
     // 출력
     for(let i = 0; i < answer.length; i++){
@@ -15,6 +17,22 @@ function main(){
 
 function solution(numbers, target) {
     let answer = 0;
+
+    // depth 가 numbers 의 index인 그래프
+    dfs(0, 0);
+
+    function dfs(depth, sum){
+        if(depth === numbers.length){
+            if(sum == target){
+                answer++;
+            }
+            return;
+        }
+
+        dfs(depth + 1, sum + numbers[depth]);
+        dfs(depth + 1, sum - numbers[depth]);
+    }
+
     return answer;
 }
 
