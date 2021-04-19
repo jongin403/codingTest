5afb6c3c9cbff49841568dbc8e6346904a324eac
jongin.jeong <jongin403@gmail.com>
2021-04-19 23:55:56 +0900

    프로그래머스 단어 변환 테스트 코드

---
+++ wordTransform.js
@@ -0,0 +1,26 @@
+// 프로그래머스
+// 코딩테스트 연습 > 깊이/너비 우선 탐색(DFS/BFS) > 단어 변환
+// 사용 알고리즘 : 
+// 시간 복잡도 : 
+
+// 다 방문했을 때
+function main(){
+    let answer = new Array();
+    
+    // 입력
+    answer.push(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]));
+    answer.push(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log"]));
+
+    
+    // 출력
+    for(let i = 0; i < answer.length; i++){
+        console.log(answer[i]);
+    }
+}
+
+function solution(begin, target, words) {
+    var answer = 0;
+    return answer;
+}
+
+main();
