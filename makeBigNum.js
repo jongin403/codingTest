1f7a09847ed67aa008ffe9bb04850615924ba03e
jongin.jeong <jongin403@naver.com>
2021-05-04 00:23:07 +0900

    프로그래머스 큰 수 만들기 오답

--- makeBigNum.js
+++ makeBigNum.js
@@ -20,37 +20,32 @@ function main(){
 
 function solution(number, k) {
     let answer = '';
+    return answer;
+}
 
-    /*
-    let numCnt = new Array(10); // i 번째 인덱스에 숫자 i 가 몇개인지 count
-
-    for(let j = 0; j < 10; j++){
-        numCnt[j] = 0;
-    }
-
-    for(let i = 0; i < number.length; i++){
-        for(let j = 0; j < 10; j++){
-            if(number[i] == j){
-                numCnt[j]++;
-            }
-        }
-    }
-
-    console.log(numCnt);
-    */
-
+// 숫자를 제거해서 섞는게 아니라 그 순서대로 사용
+function wrongSolution(number, k) {
+    let answer = '';
     let numArray = new Array();
 
     for(let i = 0; i < number.length; i++){
         numArray.push(number[i]);
     }
-    sortedNumArray = numList.sort();
-    sortedNumArray.remove()
-    console.log(sortedNumArray);
-
+    let sorted = numArray.sort(function(a,b){ return b-a; }); // 내림차순 정렬
+    //console.log("sorted : " + sorted);
     // splice : 배열의 index 도 같이 제거
     // remove : 배열의 index 유지
-
+    
+    
+    let spliced = sorted.splice(0, number.length-k);
+    //console.log("spliced : " + spliced);
+    
+    for(let i = 0; i < spliced.length; i++){
+        if(spliced[i] != "0"){
+            answer = answer + "" + spliced[i];
+        }
+    }
+    
     return answer;
 }
 
