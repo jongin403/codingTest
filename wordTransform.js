390811de18fd182a62061b0a8444bddecfa62d2b
jongin.jeong <jongin403@naver.com>
2021-09-01 00:03:56 +0900

    프로그래머스 단어 변환

--- wordTransform.js
+++ wordTransform.js
@@ -19,8 +19,63 @@ function main(){
 }
 
 function solution(begin, target, words) {
-    var answer = 0;
+    let answer = 0;
+    let answers = []; // 정답 후보
+
+    // 방문여부 초기화
+    let visited = [];
+    for(let idx = 0; idx < words.length; idx++){
+        visited.push(false);
+    }
+
+    
+    let curWord;
+
+    function dfs(word, depth){
+        
+        for(let idx = 0; idx < words.length; idx++){
+            curWord = words[idx];
+            if(target === words[idx]){
+                answer
+                break;
+            }
+            if(isChange(curWord, words[idx]) && !visited[idx]){
+
+            }
+        }
+
+        return depth;
+    }
+
+    for(let idx = 0; idx < words.length; idx++){
+        if(isChange(start, words[idx])){
+            answers.push(dfs(words[idx]), 1);
+        }
+    }
+
+    // answers 중 최소값 찾기
+    if(answers.length > 0){
+        answer = Math.min(answers);
+    }
+
     return answer;
+}
+
+// 한개의 알파벳만 차이나는지 여부
+function isChange(word1, word2){
+    let result = false;
+    let diff = 0;
+
+    for(let idx = 0; idx < word1.length; idx++){
+        if(word1[idx] !== word2[idx]){
+            diff++;
+        }
+    }
+    if(diff === 1){
+        result = true;
+    }
+
+    return result;
 }
 
 main();
