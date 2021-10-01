77da657cc4f597dc42bb9496e0bb86d6b8207810
jongin.jeong <jongin403@naver.com>
2021-10-01 23:59:18 +0900

    프로그래머스 / 큰 수 만들기 / Maximum call stack size exceeded

--- makeBigNum.js
+++ makeBigNum.js
@@ -7,9 +7,9 @@ function main(){
     let answer = new Array();
     
     // 입력
-    answer.push(solution("1924", 2));
+    //answer.push(solution("1924", 2));
     answer.push(solution("1231234", 3));
-    answer.push(solution("4177252841", 4));
+    //answer.push(solution("4177252841", 4));
 
     
     // 출력
@@ -20,49 +20,34 @@ function main(){
 
 function solution(number, k) {
     let answer = '';
-    
-    const priorityArr = getPriority(number);
-    answer = eraseK(number, priorityArr, k);
-    
+    let numberArr = number.split('');
+    console.log(`numberArr:${numberArr}`);
+   
+    let savedIdxArr = []; // 지우지 않고 살아남은 인덱스
+
+    // k 보다 작은 인덱스 중에 가장 큰 숫자 찾기
+    const eraseKtimes = function(numberArr, k){
+        // 종료 조건
+        // arr 의 길이가 k 와 같을 경우 모두 삭제
+        if(numberArr.length === k){
+            return [];
+        }
+
+        // k 번째보다 작으면서 제일 큰 숫자 찾기
+        const curMax = Math.max(...(numberArr.slice(0,k)));
+        const curMaxIdx = numberArr.indexOf(curMax);
+        
+        // 다음 eraseKtimes 세팅
+        const nextNumberArr = numberArr.slice(0, curMaxIdx + 1);
+        const nextK = k - (curMaxIdx - 1); // curMaxIdx - 1 개의 숫자 삭제
+        
+        // 연결해서 반환
+        return numberArr.slice(0,k).concat(eraseKtimes(nextNumberArr, nextK));
+    }
+
+    answer = eraseKtimes(numberArr, k);
+
     return answer;
-}
-
-const getPriority = function(number){
-    const priorityArr = new Array(number.length).fill(0);
-    let priority = 1;
-
-    for(let compareNum = 9; compareNum >= 0; compareNum--){
-        // 앞에서 부터 하면 solution("1231234", 3) // 2334
-        //for(let i = 0 ; i < number.length; i++){ 
-        for(let i = number.length - 1 ; i >= 0; i--){
-
-            //console.log(`${number[i]} / ${compareNum} / ${number[i] === compareNum}`)
-            if(Number(number[i]) === compareNum){
-                priorityArr[i] = priority;
-                priority++;                
-            }
-
-        }
-    }
-    
-    //console.log(`number:${number}`);
-    //console.log(`priorityArr:${priorityArr}`);
-
-    return priorityArr;
-}
-
-const eraseK = function(number, priorityArr, k){
-    let exceptNumber = "";
-
-    for(let i = 0 ; i < number.length; i++){
-        console.log(`${priorityArr[i]} / ${number.length - k}`)
-        if(priorityArr[i] <= number.length - k){
-            exceptNumber = exceptNumber.concat(number[i]);
-        }
-    }
-
-    //console.log(`exceptNumber:${exceptNumber}`);
-    return exceptNumber;
 }
 
 main();
