2d2240d6e4610473aa52b4648c5bd8b381593358
jongin.jeong <jongin403@naver.com>
2021-10-07 23:59:26 +0900

    프로그래머스 / 큰 수 만들기 / 제출 완료

--- makeBigNum.js
+++ makeBigNum.js
@@ -20,80 +20,26 @@ function main(){
 
 function solution(number, k) {
     let answer = '';
-    let numberArr = number.split('');
-    //console.log(`numberArr:${numberArr}`);
+    let stack = [];
 
-    const memoization = {};
+    for(let idx = 0; idx < number.length; idx++){
+        let curNumber = number[idx];
+
+        // 맨 뒤(다음에 넣을 자리)가 현재 숫자보다 작은 경우 pop
+        //console.log(`idx:${idx} / curNumber:${curNumber} / k:${k} / stack:${stack}`);
+        while(k > 0 && stack[stack.length - 1] < curNumber){
+            stack.pop();
+            k--;
+        }
+        stack.push(curNumber);
+
+    }
     
-    const erase1element = function(numberArr){
-        let curVal = numberArr[0];
-        let eraseIdx = numberArr.length - 1; // eraseIdx 를 못 찾을 경우 맨뒤를 삭제
-
-        for(let idx = 1; idx < numberArr.length; idx++){
-            // 값이 더 커지면 앞에 있는 값을 삭제
-            if(curVal < numberArr[idx]){
-                eraseIdx = idx - 1;
-                break;
-            }
-        }
-
-        const result = numberArr.slice(0, eraseIdx)
-                                .join('')
-                                .concat(numberArr.slice(eraseIdx + 1)
-                                                 .join(''));
-
-        //console.log(`${numberArr} / ${result}`)
-        return result;
-    }
-
-    // k 보다 작은 인덱스 중에 가장 큰 숫자 찾기
-    const eraseKtimes = function(numberArr, k){
-        //console.log(`${numberArr} / ${k}`);
-
-        // 종료 조건
-        // 1. 삭제할 게 없을 경우
-        if(k === 0 ){
-            return numberArr.join('');
-        }
-        // 2. k가 arr 의 길이보다 같거나 클 경우 모두 삭제
-        if(numberArr.length <= k){
-            return '';
-        }
-
-        if(k === 1 ){
-            return erase1element(numberArr);
-        }
-
-        // k + 1 번째보다 작으면서 제일 큰 숫자 찾기
-        let curMax = numberArr[0];
-        let curMaxIdx = 0;
-        for(let idx = 1; idx < k + 1; idx++){
-            if(curMax < numberArr[idx]){
-                curMax = numberArr[idx];
-                curMaxIdx = idx;
-            }
-        }
-        
-        // 다음 eraseKtimes 세팅
-        const nextNumberArr = numberArr.slice(curMaxIdx + 1);
-        const nextK = k - curMaxIdx; // curMaxIdx 개의 숫자 삭제
-        
-        // 메모이제이션 추가
-        if(!memoization[(nextNumberArr, nextK)]){
-            memoization[(nextNumberArr, nextK)] = eraseKtimes(nextNumberArr, nextK);
-        }
-        const curResult = curMax.concat(memoization[(nextNumberArr, nextK)]);
-
-        //console.log(`curMax:${curMax} / curMaxIdx:${curMaxIdx} / curResult:${curResult}`);
-        // 연결해서 반환
-        return curResult;
-    }
-
-    answer = eraseKtimes(numberArr, k);
+    // 더 없앨만한 문자가 없는 경우 뒤에서부터 없애기
+    stack.slice(0, stack.length - k);
+    answer = stack.join('');
 
     return answer;    
 }
-
-// 테스트 8, 10 > 실패 (signal: aborted (core dumped))
 
 main();
