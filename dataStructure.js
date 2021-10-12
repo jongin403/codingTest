88a54023fe943c86d6a5d42ad6975fdcc6fc37a5
jongin.jeong <jongin403@naver.com>
2021-10-12 23:58:04 +0900

    코딩테스트 / 자료구조 / heap 구현

--- dataStructure.js
+++ dataStructure.js
@@ -1,7 +1,8 @@
 // 코딩 테스트용 자료구조 구현
 
 function main(){
-    const stack = new Stack();
+    const heap = new Heap();
+    heap.print();
     
 }
 
@@ -59,7 +60,84 @@ class Stack {
 }
 
 class Heap {
+    constructor(){
+        //this.data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 0, 1];
+        this.data = [];
+    }
+
+    swap(index1, index2){
+        let temp = this.data[index1];
+        this.data[index1] = this.data[index2];
+        this.data[index2] = temp;
+    }
+
+    parentIndex(index){
+        return Math.floor((index - 1) / 2);
+    }
+
+    leftChildIndex(index){
+        return index * 2 + 1;
+    }
+
+    rightChildIndex(index){
+        return index * 2 + 2;
+    }
+
+    parent(index){
+        return this.data[this.parentIndex(index)];
+    }
+
+    leftChild(index){
+        return this.data[this.leftChildIndex(index)];
+    }
+
+    rightChild(index){
+        return this.data[this.rightChildIndex(index)];
+    }
+
+    peek(){
+        return this.data[0];
+    }
+
+    size(){
+        return this.data.length;
+    }
+
+    print(){
+        // 0 : 2^0 - 1 ~ 2^1 - 2
+        // 1 2 : 2^1 - 1 ~ 2^2 - 2
+        // 3 4 5 6 : 2^2 - 1 ~ 2^3 - 2
+        // ...
+        let printVal;
+        let curVal;
+        let exponent = 0;
         
+        while(2 ** exponent - 2 < this.size()){
+            printVal = "";
+            for(let idx = 2 ** exponent - 1; idx < 2 ** (exponent + 1) - 1; idx++){
+                
+                if(this.data[idx] != undefined){
+                    curVal = this.data[idx];
+                } else {
+                    curVal = "' '";
+                }
+                
+                if(idx == 2 ** exponent - 1){
+                    printVal += curVal;
+                } else {
+                    printVal += ", " + curVal;
+                }
+            }
+            console.log(`depth:${exponent} / ${printVal}`);
+            exponent += 1;
+        }
+    }
+}
+
+class MinHeap {
+}
+
+class MaxHeap {
 }
 
 class Queue {
