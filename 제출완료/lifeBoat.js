dccdb63bbd6e6f53b6109e2274a3a7328ee606c4
jongin.jeong <jongin403@naver.com>
2021-11-13 23:52:50 +0900

    프로그래머스 / 구명보트 / 제출 완료

--- 제출완료/lifeBoat.js
+++ 제출완료/lifeBoat.js
@@ -7,11 +7,11 @@ function main(){
     let answer = new Array();
     
     // 입력
-    //answer.push(solution([70, 50, 80, 50], 100)); // 3
-    //answer.push(solution([70, 80, 50], 100)); // 3
-    //answer.push(solution([10, 10, 20, 20, 30, 50, 40, 20, 30], 100)); // 5
+    answer.push(solution([70, 50, 80, 50], 100)); // 3
+    answer.push(solution([70, 80, 50], 100)); // 3
+    answer.push(solution([10, 10, 20, 20, 30, 50, 40, 20, 30], 100)); // 5
     answer.push(solution([40, 50, 150, 160], 200)); // 2
-    //answer.push(solution([100, 500, 500, 900, 950], 1000)); // 3
+    answer.push(solution([100, 500, 500, 900, 950], 1000)); // 3
     
     // 출력
     for(let i = 0; i < answer.length; i++){
@@ -21,7 +21,7 @@ function main(){
 
 // 몸무게 큰 순서부터 차례로 넣기
 // 작은 순서부터 할 경우 큰 몸무게에 끼워넣을 수 없으므로
-function solution(people, limit) {
+function solution2(people, limit) {
     let answer = 0;
     
     const LIMIT_PEOPLE = 2;
@@ -29,12 +29,17 @@ function solution(people, limit) {
     const movedPeople = new Array(people.length);
     movedPeople.fill(false);
 
+    const upperPeople = people.filter(a => limit / 2 < a);
+    const lowerPeople = people.filter(a => limit / 2 >= a);
+    console.log(`reverse sorted people:${people}`);
+
     people.sort((a,b) => a - b).reverse(); // sort 따로 설정하지 않으면 50,40,160,150 로 정렬
+    
     //console.log(`reverse sorted people:${people}`);
     
     let movedPeopleCnt = 0;
     let boatCnt = 0;
-/*
+
     while(true){
 
         let curLimit = limit;
@@ -65,9 +70,41 @@ function solution(people, limit) {
             break;
         }
     }
-    */
+    
     answer = boatCnt;
 
+    return answer;
+}
+
+function solution(people, limit) {
+    let answer = 0;
+    
+    let leftIdx = 0;
+    let rightIdx = people.length - 1;
+
+    people.sort((a,b) => a - b).reverse();
+
+    while(true){
+
+        let sum = people[leftIdx] + people[rightIdx];
+        
+        if(sum <= limit){
+            rightIdx--;
+        } 
+        leftIdx++;
+
+        answer++;
+
+        if(rightIdx == leftIdx){
+            answer++;
+        }
+
+        if(rightIdx <= leftIdx){
+            break;
+        }
+        
+    }
+    
     return answer;
 }
 
