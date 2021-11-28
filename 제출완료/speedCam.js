0dc444df8b833b03ee4cbb47267039fd60c1441d
jongin.jeong <jongin403@naver.com>
2021-11-28 23:25:20 +0900

    프로그래머스 / 단속카메라 / 제출 완료

--- 제출완료/speedCam.js
+++ 제출완료/speedCam.js
@@ -1,8 +1,9 @@
 // 프로그래머스
 // 코딩테스트 연습 > 탐욕법(Greedy) > 단속카메라
-// 사용 알고리즘 : 
-// 시간 복잡도 : 
+// 사용 알고리즘 : 정렬, 탐욕법
+// 시간 복잡도 : O(n^2)
 
+// 풀이
 // 정렬할 수 있는 기준을 찾아 봤을 때 도로 길이, 시작위치, 종료위치만 존재
 // 1. 종료위치 기준으로 오름차순 정렬
 // 2. 현재 도로 내에 카메라가 설치되어 있지 않다면 빠져나간 지점에 카메라를 설치
@@ -26,13 +27,21 @@ function solution(routes) {
     // 종료위치 기준 오름차순 정렬
     routes.sort((a, b) => a[1] - b[1]);
     for(let idx = 0; idx < routes.length; idx++){
-        console.log(`idx:${idx} / routes:${routes[idx]}`);
+        //console.log(`idx:${idx} / routes:${routes[idx]}`);
 
+        let isMonitored = false;
+        for(let jdx = 0; jdx < camLoc.length; jdx++){
+            if(routes[idx][0] <= camLoc[jdx] && camLoc[jdx] <= routes[idx][1]){
+                isMonitored = true;
+            }
+        }
 
+        if(!isMonitored){
+            camLoc.push(routes[idx][1]);
+        }
     }
 
-    
-    
+    answer = camLoc.length;
 
     return answer;
 }
