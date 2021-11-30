45a120e9b12203542a513227a42edea885fdf690
jongin.jeong <jongin403@naver.com>
2021-11-30 23:58:41 +0900

    프로그래머스 / 징검다리 / 개발중

--- steppingStone.js
+++ steppingStone.js
@@ -4,47 +4,42 @@
 // 시간 복잡도 : 
 
 function main(){
-    const input = new Array();
-    const output = new Array();
-    
-    // 입출력 예 입력
-    input.push([25, [2, 14, 11, 21, 17], 2]);
-    output.push(4);
     
     console.log(`execution result`);
-    for(let idx = 0; idx < input.length; idx++){
-        let start = new Date();
-        let answer = solution(input[idx])
-        let end = new Date();
-        
-        console.log(`#${idx}. answer:${answer} / correct answer:${output[idx]} / time:${end - start}ms`);
-    }
+    let start = new Date();
+    let answer = solution(25, [2, 14, 11, 21, 17], 2); // 4
+    let end = new Date();
     
+    console.log(`#answer:${answer} / time:${end - start}ms`);
 }
 
 // 이분 탐색을 하는 대상 : 바위 사이의 최소 간격
 function solution(distance, rocks, n) {
     let answer = 0;
 
+    console.log(`distance:${distance} / rocks:${rocks} / n:${n}`);
     rocks.push(0);
     rocks.push(distance);
     rocks.sort((a, b) => a - b);
 
-    //const rockDist
+    const rockDist = new Array(rocks.length - 1);
+    for(let idx = 0; idx < rocks.length - 1; idx++){
+        rockDist[idx] = rocks[idx + 1] - rocks[idx];
+    }
 
+    console.log(`rockDist:${rockDist}`);
+    
     let start = 1;
     let end = distance;
 
-    while(start < end){
-        if(true){
-            start++;
-        } else {
-            end--;
-        }
-    }
+    // while(start < end){
+    //     if(true){
+    //         start++;
+    //     } else {
+    //         end--;
+    //     }
+    // }
     
-
-
     return answer;
 }
 
