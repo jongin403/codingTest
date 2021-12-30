4b8b67d88e29d2fbb27af5d0557e1e302f346908
jongin.jeong <jongin403@naver.com>
2021-12-31 00:24:57 +0900

    Leet Code / #1. Two Sum / 제출 완료

--- leetcode/twoSum.js
+++ leetcode/twoSum.js
@@ -1,7 +1,14 @@
-// leetcode
+// Leet Code
+// #1. Two Sum
+// Success
+// Runtime: 80 ms, faster than 80.07% of JavaScript online submissions for Two Sum.
+// Memory Usage: 41.4 MB, less than 18.03% of JavaScript online submissions for Two Sum.
 
 function main(){
+    // Input
     nums = [2,7,11,15], target = 9;
+    // Output
+    // [0,1]
     console.log(twoSum(nums, target));
 }
 
@@ -10,19 +17,38 @@ function main(){
  * @param {number} target
  * @return {number[]}
  */
- var twoSum = function(nums, target) {
-    const result = [];
+// var twoSum = function(nums, target) {
+//     const result = [];
     
-    for(let idx = 0; idx < nums.length - 1; idx++){
-        for(let jdx = idx + 1; jdx < nums.length; jdx++){
-            if(nums[idx] + nums[jdx] == target){
-                result.push(idx);
-                result.push(jdx);
-            }
-        }    
-    }
+//     for(let idx = 0; idx < nums.length - 1; idx++){
+//         for(let jdx = idx + 1; jdx < nums.length; jdx++){
+//             if(nums[idx] + nums[jdx] == target){
+//                 result.push(idx);
+//                 result.push(jdx);
+//             }
+//         }    
+//     }
     
-    return result;
+//     return result;
+// };
+
+// use map because 
+// map : O(1)
+// array's for loop : O(n)
+var twoSum = function(nums, target) {
+    const map = new Map(); // key : number / value : idx
+    let answer = [];
+
+    nums.forEach(function(item, index){
+        let left = target - item;
+        if(map.has(left)){
+            answer = [map.get(left), index];
+        }
+        map.set(item, index);
+        //console.log(`item:${item} / index:${index}`);
+    } );
+
+    return answer;
 };
 
 main();
