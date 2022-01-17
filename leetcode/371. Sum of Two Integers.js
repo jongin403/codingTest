140e200a0f99ea5d9f97e4738635f45a4a375c99
jongin.jeong <jongin403@naver.com>
2022-01-18 00:47:34 +0900

    Leet Code / #371. Sum of Two Integers / 제출 완료

--- leetcode/371. Sum of Two Integers.js
+++ leetcode/371. Sum of Two Integers.js
@@ -1,9 +1,12 @@
 // Leet Code
 // #371. Sum of Two Integers
+// Success
+// Runtime: 102 ms, faster than 25.23% of JavaScript online submissions for Sum of Two Integers.
+// Memory Usage: 38 MB, less than 95.68% of JavaScript online submissions for Sum of Two Integers.
 
 function main(){
     // Input // Output
-    a = 15, b = 7; // 3
+    a = 129, b = 47; // 176
 
     console.log(getSum(a, b));
 }
@@ -13,19 +16,35 @@ function main(){
  * @param {number} b
  * @return {number}
  */
-// c : 11011110
-// a : 00100101
-// b : 01101011
-// s : 10010000
+ // a:10000001|b:  101111|a^b:10101110|a&b:       1|carry:      10
+ // a:10101110|b:      10|a^b:10101100|a&b:      10|carry:     100
+ // a:10101100|b:     100|a^b:10101000|a&b:     100|carry:    1000
+ // a:10101000|b:    1000|a^b:10100000|a&b:    1000|carry:   10000
+const pBin = function printBinary(n){
+    return n.toString(2).padStart(8);
+};
+
 var getSum = function(a, b) {
     let carry;
-    console.log(`a & b:${(a & b).toString(2)}`);
+    
     while((a & b) !== 0){
+        // divide (a & b) and (a ^ b)
+
         carry = (a & b) << 1;
-        console.log(`a:${a.toString(2)} / b:${b.toString(2)} / a & b:${(a & b).toString(2)} / carry:${carry}`);
+        // (a & b) bits are carries
+        // (a ^ b) bits remains
+        // calc carries and remains like a, b
+        console.log(`a:${pBin(a)}|b:${pBin(b)}|a^b:${pBin(a ^ b)}|a&b:${pBin(a & b)}|carry:${pBin(carry)}`);
+        
         a = a ^ b;
         b = carry;
+        // doesn't matter
+        // b = a ^ b
+        // a = carry
     }
+
+    // if a & b === 0
+    // a ^ b is same as a + b
     return a ^ b;
 };
 
