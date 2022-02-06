5c53651373f8ed2bbec82c80f1a4e3fbd97c9830
jongin.jeong <jongin403@naver.com>
2022-02-06 18:59:13 +0900

    프로그래머스 / JadenCase 문자열 만들기 / 제출 완료

--- programmers/makeJadenCase.js
+++ programmers/makeJadenCase.js
@@ -1,42 +1,32 @@
 // 프로그래머스
 // 코딩테스트 연습 > 연습문제 > JadenCase 문자열 만들기
-// 점수 : 50.0 / 50.0
 
 function main(){
-    let answer = new Array();
+    const answer = new Array();
+    const output = new Array();
     
-    // 입력
-    answer.push(solution("3people unFollowed me")); // "3people Unfollowed Me"
-    answer.push(solution("for the last week")); // "For The Last Week"
+    // 입출력 예 입력
+    answer.push(solution("3people unFollowed me"));
+    output.push("3people Unfollowed Me");
     
-    // 출력
-    for(let i = 0; i < answer.length; i++){
-        console.log(answer[i]);
+    console.log(`execution result`);
+    for(let idx = 0; idx < answer.length; idx++){
+        console.log(`#${idx}. answer:${answer[idx]} / correct answer:${output[idx]}`);
     }
+    
 }
 
 function solution(s) {
-    let answer = '';
-    //let words = s.split(' ');
-    let upperYn = false;
-
-    for(let idx = 0; idx < s.length; idx++){
-        if(idx == 0){
-            upperYn = true;
-        }
-
-        if(upperYn){
-            answer += s[idx].toUpperCase();            
-        } else {
-            answer += s[idx].toLowerCase();
-        }
-
-        if(s[idx] === ' '){
-            upperYn = true;
-        } else {
-            upperYn = false;
+    var answer = '';
+    
+    s = s.toLowerCase();
+    for(let i = 0; i < s.length; i++){
+        if(i === 0 || s.charAt(i - 1) === ' '){
+            s = s.substring(0, i).concat(s.charAt(i).toUpperCase()).concat(s.substring(i + 1));
         }
     }
+    //console.log(s);
+    answer = s;
 
     return answer;
 }
