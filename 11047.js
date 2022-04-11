a97580ee4e61c507542e02345c1aa0a319a3cdbd
jongin.jeong <jongin403@naver.com>
2022-04-11 23:21:17 +0900

    백준 / 11047 동전 0 / 개발중

---
+++ 11047.js
@@ -0,0 +1,38 @@
+// 백준
+// 문제 : 11047 동전 0
+// 티어 : 실버3
+// 사용 알고리즘 : 그리디
+// 시간 복잡도 : 
+// 
+
+const fs = require('fs');
+const path = [
+    '/dev/stdin'  // baekjoon 제출용
+  , './input.txt' // local 작업용
+];
+
+// 입력 받기
+const [n, k, ...arr] = require('fs').readFileSync(path[1])
+                              .toString()
+                              .trim()
+                              .split(/\s+/);
+
+solution(n, k, arr);
+
+function solution(n, k, arr) {
+    // 입력 확인
+    //const inputExample = `n:${n} / k:${k} / arr:${arr}`;
+    //console.log(`inputExample:${inputExample}`);
+
+    // 로직
+    let result = 0;
+    let leftK = k;
+
+    for(let idx = arr.length - 1; 0 <= idx; idx--){
+        result += Math.floor(k / arr[idx]);
+        leftK = leftK % arr[idx];
+    }
+
+    // 제출
+    console.log(result);
+}
